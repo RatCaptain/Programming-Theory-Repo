db7dad4625209fe8be77d1f0236f813880047930
RatCaptain <zsoleszguest0001@gmail.com>
2022-03-03 17:30:57 +0200

    initial commit
    -spawner skeleton

---
+++ Assets/Scripts/AnimalSpawner.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimalSpawner : MonoBehaviour
+{
+    public GameObject[] prefabs = new GameObject[2];
+
+    void Spawn()
+    {
+        
+    }
+
+
+}
