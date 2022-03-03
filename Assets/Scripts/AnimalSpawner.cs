3729059b41a50aefe6bf8a959dd775c4bdc8b919
RatCaptain <zsoleszguest0001@gmail.com>
2022-03-03 17:55:57 +0200

    initial commit
    -spawn animal type with name declared on menu

--- Assets/Scripts/AnimalSpawner.cs
+++ Assets/Scripts/AnimalSpawner.cs
@@ -1,14 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class AnimalSpawner : MonoBehaviour
 {
     public GameObject[] prefabs = new GameObject[2];
+    private GameObject animalToSpawn;
+    private GameObject animalname;
+    private TextMeshProUGUI animalnameText;
 
+    private void Start() 
+    {
+        animalDeclare();
+        Spawn();
+    }
+
+    void animalDeclare()
+    {
+        if (MenuManager.Instance.animalType == "Cat")
+        {
+            animalToSpawn = prefabs[0];
+        }
+        else
+        {
+            animalToSpawn = prefabs[1];
+        }
+    }
     void Spawn()
     {
         
+        Instantiate(animalToSpawn, new Vector3(0, 0, 0), Quaternion.identity);
+        animalname = GameObject.Find("Animal name");
+        animalnameText = animalname.GetComponent<TextMeshProUGUI>();
+        animalnameText.text = MenuManager.Instance.animalName;
     }
 
 
